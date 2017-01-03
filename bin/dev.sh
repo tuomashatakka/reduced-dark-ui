e1b620081b8c11a3b7bbf64b8c143d0ae3ea4f14
Tuomas Hatakka <tuomas.hatakka@fns.fi>
2017-01-03 05:40:28 +0200

    Major overhaul for highlight color support

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
+clear
+
 NAME=reduced-dark-ui
 PROJECT_DIR=~/Projects/Atom/$NAME
 
-echo "Navigating to the project root $PROJECT_DIR"
+# RETURNVAL=$(node -e "require('$PROJECT_DIR/bin/run.js')" $NAME)
+# echo $RETURNVAL
+# exit 1;
+
+echo "Navigating to the project root $PROJECT_DIR\n"
 cd $PROJECT_DIR
 
-echo "Unlinking the package $NAME"
+echo "Unlinking the package $NAME\n"
 apm-beta unlink $PROJECT_DIR
 apm-beta unlink $PROJECT_DIR --dev
 
-echo "Linking the package $NAME as a dev pkg"
+echo "Linking the package $NAME as a dev pkg\n"
 apm-beta link $PROJECT_DIR --dev
 
-echo "Launching atom in development mode for the project $NAME"
+echo "Launching atom in development mode for the project $NAME\n"
 atom-beta --dev $PROJECT_DIR
