09489cd9e76ce6dfd015102818eda5fce6c191e4
Tuomas Hatakka <tuomas.hatakka@aalto.fi>
2017-01-14 19:02:42 +0200

    Added a missing config to the config-fallback less file

--- bin/win.dev.sh
+++ bin/win.dev.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
 clear
+$CURRENTDIR=$(pwd)
 
 NAME=reduced-dark-ui
-PROJECT_DIR=~/Projects/Atom/$NAME
+PROJECT_DIR=$(pwd)/../$NAME
 
 # RETURNVAL=$(node -e "require('$PROJECT_DIR/bin/run.js')" $NAME)
 # echo $RETURNVAL
 # exit 1;
 
-echo "Navigating to the project root $PROJECT_DIR\n"
+echo "Navigating to the project root $PROJECT_DIR"
 cd $PROJECT_DIR
 
-echo "Unlinking the package $NAME\n"
+echo "Unlinking the package $NAME"
 apm unlink $PROJECT_DIR
 apm unlink $PROJECT_DIR --dev
 
-echo "Linking the package $NAME as a dev pkg\n"
+echo "Linking the package $NAME as a dev pkg"
 apm link $PROJECT_DIR --dev
 
-echo "Launching atom in development mode for the project $NAME\n"
+echo "Launching atom in development mode for the project $NAME"
 atom --dev $PROJECT_DIR
